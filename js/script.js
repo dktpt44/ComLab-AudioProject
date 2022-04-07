ce2f79fc957ca4a9e2a626d917ac15cb6ca04f4b
Aigerim Zhusubalieva <aigerim@Aigerims-MacBook-Pro.local>
2022-04-07 11:14:05 +0400

    hey

---
+++ js/script.js
@@ -0,0 +1,8 @@
+window.addEventListener("keydown", function(e) {
+  // space, page up, page down and arrow keys:
+  if([32, 33, 34, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+      e.preventDefault();
+  }
+}, false);
+
+window.addEventListener("wheel", e => e.preventDefault(), { passive:false })
