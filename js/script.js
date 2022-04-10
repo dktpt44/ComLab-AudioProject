6deed102208a739db195623b62667e4092e5b9c3
Aigerim Zhusubalieva <az2177@nyu.edu>
2022-04-10 16:55:02 +0400

    added pause on space bar and some lame animation (not final)

--- js/script.js
+++ js/script.js
@@ -5,11 +5,54 @@ $(document).ready(() => {
     $(".homeScreen").fadeIn("slow", () => {
       //when first screen is displayed
       // any code write here:
+
+      var sound = document.getElementById("sound");
+      var soundPlaying = false;
+      var pauseButton = document.getElementById("pause");
+      var playButton = document.getElementById("play");
+
+      let takeOffBtn = $("#takeOffBtn");
+      takeOffBtn.on("click", () => {
+        $(".takeoffclass").fadeOut();
+        sound.play();
+        soundPlaying = true;
+      });
+
+      var playSound = function(){
+        sound.play();
+        playButton.style.display = "none";
+        pauseButton.style.display = "block"
+        soundPlaying = true;
+      }
+
+      var pauseSound = function(){
+        sound.pause();
+        soundPlaying = false;
+        pauseButton.style.display = "none"
+        playButton.style.display = "block";
+      }
+
+      pauseButton.addEventListener("click", function(e){
+       pauseSound();
+      });
+
+      playButton.addEventListener("click", function(e){
+        playSound();
+      });
+
       window.addEventListener("keydown", function(e) {
         // space, page up, page down and arrow keys:
-        if([32, 33, 34, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        if([33, 34, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
             e.preventDefault();
         }
+        if([32].indexOf(e.keyCode) > -1) {
+          e.preventDefault();
+          if(soundPlaying){
+            pauseSound();
+          }else{
+            playSound();
+          }
+      }
       }, false);
       
       window.addEventListener("wheel", e => e.preventDefault(), { passive:false });
@@ -17,17 +60,19 @@ $(document).ready(() => {
       var audio = document.getElementById("sound");
       var slow = document.querySelectorAll(".slow");
       
-      
       sound.addEventListener("timeupdate", function(event){
         //get time stamp of the sound
-        if(audio.currentTime<3.0){
-          // slow.forEach(obj => {
-          //   obj.style.left = ;
-          //   obj.style.left = ;
-          // });
-          //figure out how to do thiss
+        if(audio.currentTime<30.0){
+          slow.forEach(obj => {
+            var position = obj.getBoundingClientRect();
+            obj.style.left = position.left - 30 + 'px';
+            // obj.style.top = ;
+          });
+          
         }
       });
+
+      
 
 
 
